f59b7d4f9fc2bf11d88ff46a30064d52ef2df451
chendimao <chendimao@gmail.com>
2017-03-05 21:12:43 +0800

    添加https-2017年3月5日21:12:42

--- resources/views/vendor/UEditor/head.blade.php
+++ resources/views/vendor/UEditor/head.blade.php
@@ -1,4 +1,4 @@
-<script src="{!!asset('public/laravel-u-editor/ueditor.config.js')!!}"></script>
-<script src="{!!asset('public/laravel-u-editor/ueditor.all.min.js')!!}"></script>
+<script src="{!!secure_url('public/laravel-u-editor/ueditor.config.js')!!}"></script>
+<script src="{!!secure_url('public/laravel-u-editor/ueditor.all.min.js')!!}"></script>
 {{-- 载入语言文件,根据laravel的语言设置自动载入 --}}
-<script src="{!!asset('public'.$UeditorLangFile)!!}"></script>
+<script src="{!!secure_url('public'.$UeditorLangFile)!!}"></script>
